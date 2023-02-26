0c6edc3e0d5666ed5b223808b962b6aef1a5f3a5
Alexandre <jalexandredutra@gmail.com>
2023-02-26 01:34:45 -0300

    refactor: tradução de componentes e páginas

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -20,6 +20,16 @@ interface WeatherData {
 export const Weather: React.FC<{ data: WeatherData }> = ({ data }) => {
   const { main, weather, wind, name } = data;
   const { temp, feels_like, humidity } = main;
+  const celsiusTemp = (((temp - 32) * 5) / 9).toFixed(0);
+  const kmhWindSpeed = (wind.speed * 1.60934).toFixed(0);
+  const translateWeather = (weatherValue: string) => {
+    switch (weatherValue) {
+      case "Clouds":
+        return "Nuvens";
+      default:
+        return weatherValue;
+    }
+  };
 
   return (
     <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
@@ -32,25 +42,25 @@ export const Weather: React.FC<{ data: WeatherData }> = ({ data }) => {
             width="100"
             height="100"
           />
-          <p className="text-2xl">{weather[0].main}</p>
+          <p className="text-2xl">{translateWeather(weather[0].main)}</p>
         </div>
-        <p className="text-9xl">{temp.toFixed(0)}&#176;</p>
+        <p className="text-9xl">{celsiusTemp}&#176;</p>
       </div>
       {/* Bottom */}
       <div className="bg-black/50 relative p-8 rounded-md">
-        <p className="text-2xl text-center pb-6">Weather in {name}</p>
+        <p className="text-2xl text-center pb-6">Tempo em {name}</p>
         <div className="flex justify-between text-center">
           <div>
-            <p className="font-bold text-2xl">{feels_like.toFixed(0)}&#176;</p>
-            <p className="text-xl">Feels Like</p>
+            <p className="font-bold text-2xl">{celsiusTemp}&#176;</p>
+            <p className="text-xl">Sensação</p>
           </div>
           <div>
             <p className="font-bold text-2xl">{humidity}%</p>
-            <p className="text-xl">Humidity</p>
+            <p className="text-xl">Umidade</p>
           </div>
           <div>
-            <p className="font-bold text-2xl">{wind.speed.toFixed(0)} MPH</p>
-            <p className="text-xl">Winds</p>
+            <p className="font-bold text-2xl">{kmhWindSpeed} Km/h</p>
+            <p className="text-xl">Ventos</p>
           </div>
         </div>
       </div>
