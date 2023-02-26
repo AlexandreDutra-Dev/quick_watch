f52b97346929762e741e4db965d208107f5aad16
Alexandre <jalexandredutra@gmail.com>
2023-02-25 22:06:07 -0300

    feat(next-image): atualiza para legacy-image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { BsSearch } from "react-icons/bs";
